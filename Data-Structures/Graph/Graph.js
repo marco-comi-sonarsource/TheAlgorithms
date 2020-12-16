090fe8c585064e4f1f0422729276b8feb17c001e
marco.comi <marco.comi@sonarsource.com>
2020-12-16 13:59:53 +0100

    idsdsd

--- Data-Structures/Graph/Graph.js
+++ Data-Structures/Graph/Graph.js
@@ -8,12 +8,6 @@ class Graph {
   }
 
   containsVertex (vertex) {
-	const Formidable = require('formidable');
-	
-	const form = new Formidable(); // Noncompliant, this form is not safe
-	form.uploadDir = ""; // because upload dir is not defined (by default os temp dir: /var/tmp or /tmp)
-	form.keepExtensions = true; // and file extensions are kept
-	
     return typeof (this.adjacencyMap[vertex]) !== 'undefined'
   }
 

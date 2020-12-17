64c89eb62b5869649f904f76b06c46bbd9a3fcb3
marco.comi <marco.comi@sonarsource.com>
2020-12-17 14:56:51 +0100

    dkdkk

--- Data-Structures/Linked-List/DoublyLinkedList.js
+++ Data-Structures/Linked-List/DoublyLinkedList.js
@@ -5,19 +5,7 @@ function DoubleLinkedList () {
     this.next = null
     this.prev = null
   }
-
-
-
-  const fs = require('fs');
-
-  function (req, res) {
-    const reqPath = __dirname + req.query.filename; // user-controlled path
   
-    let data = fs.readFileSync(reqPath, { encoding: 'utf8', flag: 'r' }); // Noncompliant
-  }
-
-
-
   let length = 0
   let head = null
   let tail = null

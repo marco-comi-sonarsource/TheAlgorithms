1a4e2dd7db31dca78865c7739d7c1b0c91d77179
marco.comi <marco.comi@sonarsource.com>
2020-12-17 15:09:38 +0100

    ssdksdk

--- Data-Structures/Heap/MaxHeap.js
+++ Data-Structures/Heap/MaxHeap.js
@@ -7,11 +7,6 @@ class BinaryHeap {
   constructor () {
     this.heap = []
 
-    const Formidable = require('formidable');
-
-const form = new Formidable(); // Noncompliant, this form is not safe
-form.uploadDir = ""; // because upload dir is not defined (by default os temp dir: /var/tmp or /tmp)
-form.keepExtensions = true; // and file extensions are kept
   }
 
   insert (value) {

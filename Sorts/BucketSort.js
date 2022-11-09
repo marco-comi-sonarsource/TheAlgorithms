c1c6c6d7ebc248814adb918b12fa78b36f334eb9
Marco Comi <marco.comi@sonarsource.com>
2022-11-09 10:16:48 +0100

    test

--- Sorts/BucketSort.js
+++ Sorts/BucketSort.js
@@ -54,7 +54,9 @@ function bucketSort (list, size) {
 }
 
 // Testing
-const arrOrignal = [5, 6, 7, 8, 1, 2, 12, 14]
+
+const arrOrignal = [5, 6, 7, 8, 1, 2, 12, 14];
+arrOrignal.sort();
 // > bucketSort(arrOrignal)
 // [1, 2, 5, 6, 7, 8, 12, 14]
 // Array before Sort
